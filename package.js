068eb46a053d3b736ac9bc0b6334e5bdb50363a1
Ryan Swapp <ryancswapp@gmail.com>
2015-04-28 18:03:21 -0600

    Added github repo to package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A meteor wrapper for the spectrum colorpicker',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ryanswapp/ryanswapp-spectrum-colorpicker.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
